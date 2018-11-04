285d985873516d65f795e06ff6a2fde60fc2d002
DANG Fan <dangf09@gmail.com>
2018-11-05 00:03:30 +0800

    ADD transceive

--- app/src/main/java/im/nfc/nfsee/nfc/LuaExecutor.kt
+++ app/src/main/java/im/nfc/nfsee/nfc/LuaExecutor.kt
@@ -1,11 +1,12 @@
 package im.nfc.nfsee.nfc
 
+import org.luaj.vm2.LuaValue
 import org.luaj.vm2.lib.jse.JsePlatform
 
 object LuaExecutor {
-    fun execute(script: String) {
+    fun execute(script: String): LuaValue {
         val globals = JsePlatform.standardGlobals()
         val chunk = globals.load(script)
-        chunk.call()
+        return chunk.call()
     }
 }
--- app/src/main/java/smartcard.kt
+++ app/src/main/java/smartcard.kt
@@ -1,12 +1,18 @@
+import android.nfc.tech.IsoDep
 import im.nfc.nfsee.utils.ByteUtils.hexToBytes
 import im.nfc.nfsee.utils.ByteUtils.toHexString
 import im.nfc.nfsee.utils.Crypto
 import org.luaj.vm2.LuaValue
+import org.luaj.vm2.lib.OneArgFunction
 import org.luaj.vm2.lib.ThreeArgFunction
 import org.luaj.vm2.lib.TwoArgFunction
 import java.lang.Exception
 
 class smartcard : TwoArgFunction() {
+    companion object {
+        var card: IsoDep? = null
+    }
+
     override fun call(modname: LuaValue, env: LuaValue): LuaValue {
         val library = LuaValue.tableOf()
         library.set("des", des())
@@ -14,6 +20,7 @@ class smartcard : TwoArgFunction() {
         library.set("pbocenc", pbocenc())
         library.set("pbocderive", pbocderive())
         library.set("pbocmac", pbocmac())
+        library.set("transceive", transceive())
         env.set("smartcard", library)
         env.get("package").get("loaded").set("smartcard", library)
         return library
@@ -103,6 +110,16 @@ class smartcard : TwoArgFunction() {
                 val dataByteArray = data.checkjstring().hexToBytes()
                 val ret = Crypto.pbocTdesMac(keyByteArray, ivByteArray, dataByteArray)
                 LuaValue.valueOf(ret.toHexString())
+            } catch (ex: Exception) {
+                LuaValue.NIL
+            }
+        }
+    }
+
+    class transceive : OneArgFunction() {
+        override fun call(capdu: LuaValue): LuaValue {
+            return try {
+                LuaValue.valueOf(smartcard.card?.transceive(capdu.checkjstring().hexToBytes())!!.toHexString())
             } catch (ex: Exception) {
                 LuaValue.NIL
             }
