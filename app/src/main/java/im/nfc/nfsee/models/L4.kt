c3cdcc0ec9a966a167431a20827d28063eeff2c5
z4yx <z4yx@users.noreply.github.com>
2018-11-08 13:36:04 +0800

    add script for Tsinghua Campus card

--- app/src/main/java/im/nfc/nfsee/models/L4.kt
+++ app/src/main/java/im/nfc/nfsee/models/L4.kt
@@ -24,6 +24,34 @@ class L4Module {
                           table = { ['卡号'] = number, ['余额'] = balance },
                           records = {}
                         }
+                    """.trimIndent()),
+                    Script("清华大学一卡通", 1, """
+                        require 'sc'
+                        -- info = sc.transceive('00A404000A535A594B542E524F4F54')
+                        -- if not sc.isok(info) then return nil end
+                        info = sc.transceive('00B0950021')
+                        if not sc.isok(info) then return nil end
+                        number = string.sub(info, 13, 20)
+                        dueDate = string.sub(info, 25, 30)
+                        writtenDueDate = string.sub(info, 31, 36)
+                        info = sc.transceive('00A4040009A00000000386980701')
+                        if not sc.isok(info) then return nil end
+                        info = sc.transceive('00B0960027')
+                        if not sc.isok(info) then return nil end
+                        balance_resp = sc.transceive('805C000104')
+                        name = sc.parsegbk(string.sub(info, 1, 40))
+                        stuNum = sc.parseutf8(string.sub(info, 57, 76))
+                        balance = tostring(sc.hextoint(string.sub(balance_resp, 1, 8)) / 100)..'元'
+                        return {
+                          table = {
+                            ['卡号'] = number,
+                            ['实际有效期'] = "20"..dueDate,
+                            ['卡面有效期'] = "20"..writtenDueDate,
+                            ['姓名'] = name,
+                            ['学号/工号'] = stuNum,
+                            ['余额'] = balance },
+                          records = {}
+                        }
                     """.trimIndent())
             ).saveAll()
         }
