abdb0aca0609badc724e73ea8689ce52feea093b
Harry Chen <i@harrychen.xyz>
2018-11-30 16:21:47 +0800

    Move actions of UserScriptActivity to menubar, make yx happy
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- app/src/main/java/im/nfc/nfsee/activities/UserScriptActivity.kt
+++ app/src/main/java/im/nfc/nfsee/activities/UserScriptActivity.kt
@@ -4,12 +4,25 @@ import android.content.Intent
 import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.os.Bundle
-import android.widget.TextView
+import android.text.InputType
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
 import android.widget.Toast
+import com.vicpin.krealmextensions.transaction
+import im.nfc.nfsee.R
 import im.nfc.nfsee.fragments.UserScriptExecutionFragment
 import im.nfc.nfsee.fragments.UserScriptListFragment
+import im.nfc.nfsee.models.UserScript
 import im.nfc.nfsee.nfc.NfcManager
+import io.realm.Realm
+import io.realm.kotlin.createObject
 import kotlinx.android.synthetic.main.activity_viewpager.*
+import org.jetbrains.anko.*
+import org.jetbrains.anko.support.v4.alert
+import org.jetbrains.anko.support.v4.toast
+import org.joda.time.DateTime
+import java.util.*
 
 class UserScriptActivity: ViewPagerActivity(
         "脚本工具",
@@ -19,23 +32,94 @@ class UserScriptActivity: ViewPagerActivity(
     private var script = ""
 
     private lateinit var nfc: NfcManager
-    private lateinit var output: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         nfc = NfcManager(this)
     }
 
-    fun updateScript(script: String) {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_user_script, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        val executionFragment = (fragments[0] as UserScriptExecutionFragment)
+
+        when (item!!.itemId) {
+
+            R.id.menu_item_execute -> {
+                try {
+                    val params = executionFragment.params.split(',').toTypedArray()
+                    val formattedScript = executionFragment.script.format(*params)
+                    updateScript(formattedScript, false)
+                } catch (e: IllegalFormatException) {
+                    toast("设置脚本失败！请确认参数与脚本中一致")
+                }
+            }
+
+            R.id.menu_item_save -> {
+                info_viewpager.currentItem = 0
+
+                with (executionFragment) {
+                    if (script.isBlank()) {
+                        toast("脚本内容为空")
+                        return@with
+                    }
+                    alert {
+
+                        lateinit var editTextName: EditText
+
+                        title = "保存脚本"
+
+                        customView {
+                            linearLayout {
+                                padding = 20
+                                editText {
+                                    hint = getText(R.string.need_valid_filename)
+                                    inputType = InputType.TYPE_CLASS_TEXT
+                                    editTextName = this
+                                }
+                            }
+                        }
+
+                        yesButton {
+                            // FIXME: not working
+                            if (editTextName.text.toString()
+                                            .contains(Regex.fromLiteral("\\s"))) {
+                                toast("文件名不合法")
+                                return@yesButton
+                            }
+
+                            Realm.getDefaultInstance().transaction { realm ->
+                                val scriptToSave = realm
+                                        .createObject<UserScript>(DateTime.now().millis)
+                                scriptToSave.title = editTextName.text.toString()
+                                scriptToSave.script = script
+                            }
+                            toast("保存成功")
+                            (fragments[1] as UserScriptListFragment).notifyDataChanged()
+                            it.dismiss()
+                        }
+
+                    }.show()
+                }
+
+            }
+            else -> {}
+        }
+        return true
+    }
+
+
+    fun updateScript(script: String, setToUI: Boolean=true) {
         info_viewpager.currentItem = 0
         this.script = script
-        (fragments[0] as UserScriptExecutionFragment).setScript(script)
+        if (setToUI) (fragments[0] as UserScriptExecutionFragment).script = script
         Toast.makeText(this@UserScriptActivity, "请贴卡", Toast.LENGTH_SHORT).show()
     }
 
-    fun notifyDataChanged() {
-        (fragments[1] as UserScriptListFragment).notifyDataChanged()
-    }
 
     override fun initFragments() {
         fragments.add(UserScriptExecutionFragment())
@@ -54,8 +138,7 @@ class UserScriptActivity: ViewPagerActivity(
 
     override fun onNewIntent(intent: Intent) {
         val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
-        val output = nfc.executeScript(tag, script)
-        (fragments[0] as UserScriptExecutionFragment).setOutput(output)
+        (fragments[0] as UserScriptExecutionFragment).output = nfc.executeScript(tag, script)
         Toast.makeText(this, "执行完毕！", Toast.LENGTH_SHORT).show()
     }
 }
